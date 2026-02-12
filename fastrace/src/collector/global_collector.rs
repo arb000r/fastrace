7ff24dd6725f7ba6ab843d2db2a7b63703dede0f
Ansaf Ahmed <me@ansaf.net>
2026-02-12 11:45:02 +1100

    logs

--- fastrace/src/collector/global_collector.rs
+++ fastrace/src/collector/global_collector.rs
@@ -92,6 +92,7 @@ pub fn flush() {
         #[cfg(all(not(target_family = "wasm"), feature = "native-collect"))]
         {
             if let Some(collector) = GLOBAL_COLLECTOR.lock().as_mut() {
+                println!("Flushing on same thrad.");
                 collector.handle_commands();
             }
         }
